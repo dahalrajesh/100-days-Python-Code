50bfab103d3965d0b6e056a6559d5dc2410ba809
Rajesh Dahal <150427790+dahalrajesh@users.noreply.github.com>
2025-01-04 23:04:36 +0530

    Update and rename Q2.py to celsiusTOfahrenheit.py
    Write a program that will convert celsius value to fahrenheit

---
+++ celsiusTOfahrenheit.py
@@ -0,0 +1,7 @@
+#Write a program that will convert celsius value to fahrenheit
+print("Before Conversion:")
+user_input=int(input("Enter the Celsius Value:"))
+print(f"The value of Celsius is {user_input}C")
+Fahrenheit=(9/5*user_input)+32
+print("After Conversion:")
+print(f"The value of Fahrenheit is {Fahrenheit}")
